569cdf133e5908e2221a5fa0d3f32caaf3e7f58f
=김지용 <znzld505@naver.com>
2019-09-16 17:38:45 +0900

    Updates

---
+++ precache-manifest.b62a6ad585b4ab74ef36009806742669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c34eb37f6a9d5604a0afb89cea4ab74",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "448e06964417e06f0362",
+    "url": "/movieapp/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "3c82bb3f4b85ce4ba634",
+    "url": "/movieapp/static/js/2.563a7adb.chunk.js"
+  },
+  {
+    "revision": "448e06964417e06f0362",
+    "url": "/movieapp/static/js/main.d6c01c52.chunk.js"
+  },
+  {
+    "revision": "52ed4abd42e5bc3479a7",
+    "url": "/movieapp/static/js/runtime~main.fbb478a5.js"
+  }
+]);
